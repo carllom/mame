fda5cca3726684fc656a1e0be3141886ebb9e35f
Carl Lom <carl@casalom.se>
2021-03-31 00:55:56 +0200

    Ported my SA-16 implementation. Hookup of analog channels.

--- src/mame/audio/sa16.h
+++ src/mame/audio/sa16.h
@@ -5,16 +5,21 @@
  *      Author: Carl Lom
  */
 
-#ifndef SA16_H_
-#define SA16_H_
+#ifndef MAME_MACHINE_SA16_H
+#define MAME_MACHINE_SA16_H
+
+#pragma once
 
 #include "emu.h"
 
-#define MCFG_SA16_ADD(_tag, _clock, _waveram_map)  \
-	MCFG_DEVICE_ADD(_tag, SA16, _clock) \
-	MCFG_DEVICE_DATA_MAP(_waveram_map)
+//**************************************************************************
+//  TYPE DEFINITIONS
+//**************************************************************************
 
-class sa16_device: public device_t, public device_memory_interface/*, public device_sound_interface*/ {
+// ======================> sa16_base_device
+
+class sa16_base_device : public device_t, public device_memory_interface
+{
 public:
 	sa16_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
 
@@ -22,11 +27,14 @@ public:
 	DECLARE_WRITE8_MEMBER(portb_w);
 
 protected:
-	// device-level overrides
-	virtual void device_config_complete();
-	virtual void device_start();
+	// base type constructor
+	sa16_base_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, u32 clock);
 
-	virtual void device_reset();
+	// device_config_memory_interface overrides
+	virtual space_config_vector memory_space_config() const override;
+ 
+	// address space configurations
+	const address_space_config m_space_config;
 
 	// device_config_memory_interface overrides
 	virtual const address_space_config *memory_space_config(address_spacenum spacenum = AS_DATA) const;
@@ -39,25 +47,32 @@ protected:
 	//		stream_sample_t **inputs, stream_sample_t **outputs, int samples);
 
 private:
-	UINT16 wram_r16(offs_t offset) { return this->space(AS_DATA).read_word(offset); }
-	void wram_w16(offs_t offset, UINT16 data) { this->space(AS_DATA).write_word(offset, data); }
-	UINT8 wram_r8(offs_t offset) { return this->space(AS_DATA).read_byte(offset); }
-	void wram_w8(offs_t offset, UINT8 data) { this->space(AS_DATA).write_byte(offset, data); }
 
-	UINT16 m_active_channels;
+	void sa16(address_map &map);
+
+	// line callbacks
+	devcb_write_line m_int_callback;
+	devcb_write_line m_sh_callback;
+
+	u16 wram_r16(offs_t offset) { return this->space().read_word(offset); }
+	void wram_w16(offs_t offset, u16 data) { this->space().write_word(offset, data); }
+	u8 wram_r8(offs_t offset) { return this->space().read_byte(offset); }
+	void wram_w8(offs_t offset, u8 data) { this->space().write_byte(offset, data); }
+
+	// internal state (TODO)
+	u16 m_active_channels;
 
 	int m_reg800_woffset; // offset for reg writes???
 	int m_reg800_roffset; // offset for reg reads???
 	int m_blockoffset; // Block offset for WaveRAM port
 	int m_smpcounter; // Sample port counter
-	UINT16 m_port_wram; // WaveRAM port value (
-	UINT16 m_port_smp16; // Sample port value (16-bit)
+	u16 m_port_wram; // WaveRAM port value (
+	u16 m_port_smp16; // Sample port value (16-bit)
 
-	UINT8 m_regs[9]; // Regs @ offset 0
-	UINT8 m_ports[8]; // Ports @ offset 400h
-	UINT16 m_regs800[2048];
+	u8 m_regs[9]; // Regs @ offset 0
+	u8 m_ports[8]; // Ports @ offset 400h
+	u16 m_regs800[2048];
 
-	//UINT8 m_snd_regs[0x30];
 };
 
 enum SA16_REGS {
@@ -77,6 +92,26 @@ enum SA16_PORTS {
 	SA16_SMP_HIGH = 5
 };
 
-extern const device_type SA16;
+// ======================> rf5c36_device
 
-#endif /* SA16_H_ */
+class rf5c36_device : public sa16_base_device
+{
+public:
+	// device type constructor
+	rf5c36_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+};
+
+// ======================> sa16_device
+
+class sa16_device : public sa16_base_device
+{
+public:
+	// device type constructor
+	sa16_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+};
+
+// device type declarations
+DECLARE_DEVICE_TYPE(RF5C36, rf5c36_device)
+DECLARE_DEVICE_TYPE(SA16, sa16_device)
+
+#endif // MAME_MACHINE_SA16_H
