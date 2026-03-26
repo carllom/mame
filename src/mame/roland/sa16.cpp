5aace8a0b231a78a4f1d05da291df0ec88f39863
Carl Lom <carl@casalom.se>
2026-03-26 19:05:17 +0100

    zone attribute.

--- src/mame/roland/sa16.cpp
+++ src/mame/roland/sa16.cpp
@@ -22,6 +22,10 @@
 //  GLOBAL VARIABLES
 //**************************************************************************
 
+#define _BLKOFF (m_regs[SA16REG_BLK] << 13)
+#define LOG_REG_ACCESS 0  // TODO: re-enable when working on SA-16 emulation
+#define LOG_WRAM_ACCESS 0 // TODO: re-enable when working on SA-16 emulation
+
 // device type definitions
 DEFINE_DEVICE_TYPE(RF5C36, rf5c36_device, "rf5c36", "Roland RF5C36 Sampler")
 DEFINE_DEVICE_TYPE(SA16, sa16_device, "sa16", "Roland SA-16 Sampler")
@@ -87,7 +91,60 @@ void sa16_base_device::device_reset()
 
 u8 sa16_base_device::read(offs_t offset)
 {
-	return 0;
+	u8 value = -1;
+
+	// 0x1000+: Set byte offset within block
+	if (offset >= 0x1000) {
+		m_blockoffset = offset - 0x1000;
+		return 0;
+	} else if (offset >= 0x800) { // 800h+ Set register offset
+		m_reg800_roffset = offset - 0x800;
+		if (LOG_REG_ACCESS) logerror("%s%s offsetRegister => %03x\n", machine().describe_context(), tag(), m_reg800_roffset);
+		return 0;
+	}
+
+    // abc def <= ab cf de
+	switch(offset)
+	{
+	case 0: // Play tone/channel
+		value = m_active_channels;
+		if (LOG_REG_ACCESS) logerror("%s%s active_channels[0..7] => %02x\n", machine().describe_context(), tag(), value);
+		break;
+	case 1: // Play tone/channel
+		value = m_active_channels >> 8;
+		if (LOG_REG_ACCESS) logerror("%s%s active_channels[8..F] => %02x\n", machine().describe_context(), tag(), value);
+		break;
+	case 2:
+		value = m_regs800[m_reg800_roffset];
+		if (LOG_REG_ACCESS) logerror("%s%s regs800[%03x].lo => %02x\n", machine().describe_context(), tag(), m_reg800_roffset, value);
+		break;
+	case 3:
+		value = m_regs800[m_reg800_roffset] >> 8;
+		if (LOG_REG_ACCESS) logerror("%s%s regs800[%03x].hi => %02x\n", machine().describe_context(), tag(), m_reg800_roffset, value);
+		break;
+	case 4: // Waveram port low
+		value = wram_r8(_BLKOFF + m_blockoffset);
+		if (LOG_WRAM_ACCESS) logerror("%s%s WRAM[%08x].lo => %02x\n", machine().describe_context(), tag(), _BLKOFF + m_blockoffset, value);
+		break;
+	case 5: // Waveram port high
+		value = wram_r8(_BLKOFF + m_blockoffset + 1);
+		if (LOG_WRAM_ACCESS) logerror("%s%s WRAM[%08x].hi => %02x\n", machine().describe_context(), tag(), _BLKOFF + m_blockoffset, value);
+		break;
+	case 8:
+		value = m_regs[SA16REG_BLK];
+		break;
+	case 0x404: // Sample port low (16-bit value)
+		value = wram_r16(_BLKOFF + (m_smpcounter<<1)) << 4 ;
+		break;
+	case 0x405: // Sample port high (16-bit value)
+		value = wram_r16(_BLKOFF + (m_smpcounter<<1)) >> 4 ;
+		m_smpcounter++; // ??
+		break;
+	default:
+		if (LOG_REG_ACCESS) logerror("%s%s read from address %04x unimplemented\n", machine().describe_context(), tag(), offset);
+		value = 0;
+	}
+	return value;
 }
 
 
@@ -97,4 +154,57 @@ u8 sa16_base_device::read(offs_t offset)
 
 void sa16_base_device::write(offs_t offset, u8 data)
 {
+	// logerror("%s%s args: offset=%08x, data=%02x\n", machine().describe_context(), tag(), offset, data);
+	if (offset >= 0x1000) { // 1000h+: Set offset within block
+		m_blockoffset = (offset - 0x1000) << 1;
+		return;
+	} else if (offset >= 0x800) { // 800h+ Set "800"-register number to be written using regs800[] port below
+		m_reg800_woffset = offset - 0x800;
+		if (LOG_REG_ACCESS) logerror("%s%s offsetRegister <= %02x\n", machine().describe_context(), tag(), m_reg800_woffset);
+		return;
+	}
+
+	switch(offset)
+	{
+	case 0: // Play tone/channel
+		m_active_channels = (m_active_channels & 0xFF00) | data;
+		if (LOG_REG_ACCESS) logerror("%s%s active_channels[0..7] <= %02x\n", machine().describe_context(), tag(), data);
+		break;
+	case 1: // Play tone/channel
+		m_active_channels = (m_active_channels & 0x00FF) | (data << 8);
+		if (LOG_REG_ACCESS) logerror("%s%s active_channels[8..F] <= %02x\n", machine().describe_context(), tag(), data);
+		break;
+	case 2:
+		if (LOG_REG_ACCESS) logerror("%s%s regs800[%03x].lo <= %02x\n", machine().describe_context(), tag(), m_reg800_woffset, data);
+		m_regs800[m_reg800_woffset] = (0xFF00 & m_regs800[m_reg800_woffset]) | data;
+		break;
+	case 3:
+		if (LOG_REG_ACCESS) logerror("%s%s regs800[%03x].hi <= %02x\n", machine().describe_context(), tag(), m_reg800_woffset, data);
+		m_regs800[m_reg800_woffset] = (0x00FF & m_regs800[m_reg800_woffset]) | data<<8;
+		break;
+	case 4:
+		if (LOG_WRAM_ACCESS) logerror("%s%s WRAM[%08x].lo <= %02x\n", machine().describe_context(), tag(), _BLKOFF + m_blockoffset, data);
+		wram_w8(_BLKOFF + m_blockoffset, data);
+		break;
+	case 5:
+		if (LOG_WRAM_ACCESS) logerror("%s%s WRAM[%08x].hi <= %02x\n", machine().describe_context(), tag(), _BLKOFF + m_blockoffset, data);
+		wram_w8(_BLKOFF + m_blockoffset + 1, data);
+		break;
+	case 8:
+		if (LOG_REG_ACCESS) logerror("%s%s set block = %04x\n", machine().describe_context(), tag(), data);
+		m_regs[SA16REG_BLK] = data;
+		m_smpcounter = 0;
+		break;
+	case 0x404: // Sample port low (16-bit value)
+		m_port_smp16 = (0xFF00 & m_port_smp16) | data;
+		break;
+	case 0x405: // Sample port high (16-bit value)
+		m_port_smp16 = (0x00FF & m_port_smp16) | data<<8;
+		if (LOG_REG_ACCESS) logerror("%s%s sample port write[%08x] <= %04x\n", machine().describe_context(), tag(), _BLKOFF + (m_smpcounter<<1), m_port_smp16 >> 4);
+		wram_w16(_BLKOFF + (m_smpcounter<<1), m_port_smp16 >> 4);
+		m_smpcounter++;
+		break;
+	default:
+		if (LOG_REG_ACCESS) logerror("%s%s write to address %04x unimplemented (data=%02x)\n", machine().describe_context(), tag(), offset, data);
+	}
 }
